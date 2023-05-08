233d14fe4d7512985d913aa6ded053a17445306a
romani22 <you@example.com>
2023-05-08 18:30:42 -0300

    cambio menu

--- src/components/MenuFooter/index.jsx
+++ src/components/MenuFooter/index.jsx
@@ -16,64 +16,53 @@ const MenuFooter = ({ clickView }) => {
 		clickView(viewSelect);
 	}, [viewSelect]);
 	return (
-		<View style={styles.buttonUbication}>
-			<FlatList
-				ListFooterComponent={
-					<View style={styles.containerButton}>
-						<Button
-							buttonStyle={styles.Button}
-							actionPress={() => {
-								HandleclickMenu('menu');
-							}}
-						>
-							<FontAwesome5 name="list" size={24} style={styles.shadowIcon} color={colors.white} />
-							<Text style={styles.textButton}>Menu</Text>
-						</Button>
-						<Button
-							buttonStyle={styles.Button}
-							actionPress={() => {
-								HandleclickMenu('notification');
-							}}
-						>
-							<FontAwesome5 name="bell" size={24} style={styles.shadowIcon} color={colors.white} />
-							<Text style={styles.textButton}>Notification</Text>
-						</Button>
-						<Button
-							buttonStyle={styles.Button}
-							actionPress={() => {
-								HandleclickMenu('home');
-							}}
-						>
-							<MaterialCommunityIcons
-								name="home-outline"
-								size={27}
-								style={styles.shadowIcon}
-								color={colors.white}
-							/>
-							<Text style={styles.textButton}>Home</Text>
-						</Button>
-						<Button
-							buttonStyle={styles.Button}
-							actionPress={() => {
-								HandleclickMenu('invoice');
-							}}
-						>
-							<FontAwesome5 name="file-alt" size={24} style={styles.shadowIcon} color={colors.white} />
+		<View style={styles.containerButton}>
+			<Button
+				buttonStyle={styles.Button}
+				actionPress={() => {
+					HandleclickMenu('menu');
+				}}
+			>
+				<FontAwesome5 name="list" size={24} style={styles.shadowIcon} color={colors.white} />
+				<Text style={styles.textButton}>Menu</Text>
+			</Button>
+			<Button
+				buttonStyle={styles.Button}
+				actionPress={() => {
+					HandleclickMenu('notification');
+				}}
+			>
+				<FontAwesome5 name="bell" size={24} style={styles.shadowIcon} color={colors.white} />
+				<Text style={styles.textButton}>Notification</Text>
+			</Button>
+			<Button
+				buttonStyle={styles.Button}
+				actionPress={() => {
+					HandleclickMenu('home');
+				}}
+			>
+				<MaterialCommunityIcons name="home-outline" size={27} style={styles.shadowIcon} color={colors.white} />
+				<Text style={styles.textButton}>Home</Text>
+			</Button>
+			<Button
+				buttonStyle={styles.Button}
+				actionPress={() => {
+					HandleclickMenu('invoice');
+				}}
+			>
+				<FontAwesome5 name="file-alt" size={24} style={styles.shadowIcon} color={colors.white} />
 
-							<Text style={styles.textButton}>Factura</Text>
-						</Button>
-						<Button
-							buttonStyle={styles.Button}
-							actionPress={() => {
-								HandleclickMenu('turn');
-							}}
-						>
-							<FontAwesome5 name="calendar-alt" size={24} style={styles.shadowIcon} color={colors.white} />
-							<Text style={styles.textButton}>Turnos</Text>
-						</Button>
-					</View>
-				}
-			/>
+				<Text style={styles.textButton}>Factura</Text>
+			</Button>
+			<Button
+				buttonStyle={styles.Button}
+				actionPress={() => {
+					HandleclickMenu('turn');
+				}}
+			>
+				<FontAwesome5 name="calendar-alt" size={24} style={styles.shadowIcon} color={colors.white} />
+				<Text style={styles.textButton}>Turnos</Text>
+			</Button>
 		</View>
 	);
 };
